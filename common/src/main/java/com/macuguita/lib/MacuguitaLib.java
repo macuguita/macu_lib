3e5986a230bc10e035c7df39a460e9f27c568b59
macuguita <raulpripri@gmail.com>
2025-03-11 01:16:54 +0100

    VERSION 1.0.2

--- common/src/main/java/com/macuguita/lib/MacuguitaLib.java
+++ common/src/main/java/com/macuguita/lib/MacuguitaLib.java
@@ -1,6 +1,11 @@
 package com.macuguita.lib;
 
+import com.macuguita.lib.platform.registry.GuitaRegistries;
+import com.macuguita.lib.platform.registry.GuitaRegistry;
+import com.macuguita.lib.platform.registry.GuitaRegistryEntry;
 import com.macuguita.lib.supporters.RoleChecker;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +14,12 @@ public final class MacuguitaLib {
     public static final String MOD_ID = "macu_lib";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final GuitaRegistry<Item> ITEMS = GuitaRegistries.create(Registries.ITEM, MOD_ID);
+
+    public static final GuitaRegistryEntry<Item> CUSTOM_ITEM = ITEMS.register("custom", () -> new Item(new Item.Settings()));
+
     public static void init() {
+        ITEMS.init();
         RoleChecker.init();
     }
 

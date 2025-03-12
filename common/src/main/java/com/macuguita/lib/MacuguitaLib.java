d415dff339ebcd6b75cbf124f8b9a1d1d5c92b30
macuguita <raulpripri@gmail.com>
2025-03-12 01:13:05 +0100

    Item groups and removed test item

--- common/src/main/java/com/macuguita/lib/MacuguitaLib.java
+++ common/src/main/java/com/macuguita/lib/MacuguitaLib.java
@@ -1,11 +1,6 @@
 package com.macuguita.lib;
 
-import com.macuguita.lib.platform.registry.GuitaRegistries;
-import com.macuguita.lib.platform.registry.GuitaRegistry;
-import com.macuguita.lib.platform.registry.GuitaRegistryEntry;
 import com.macuguita.lib.supporters.RoleChecker;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +9,7 @@ public final class MacuguitaLib {
     public static final String MOD_ID = "macu_lib";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final GuitaRegistry<Item> ITEMS = GuitaRegistries.create(Registries.ITEM, MOD_ID);
-
-    public static final GuitaRegistryEntry<Item> TEST = ITEMS.registerRegistryEntry("test", () -> new Item(new Item.Settings()));
-
     public static void init() {
-        ITEMS.init();
         RoleChecker.init();
     }
 
